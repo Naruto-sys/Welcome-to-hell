4fb877855e8e9208de080c9eeb6ce33f28f45c74
vall-valentine <vallvalentine77@yandex.ru>
2020-01-12 21:43:22 +0300

    Some PEP8 corrections in Button

--- Button.py
+++ Button.py
@@ -6,28 +6,29 @@ class Button:
     def create_button(self, surface, color, x, y, length, height, width, text, text_color):
         surface = self.draw_button(surface, color, length, height, x, y, width)
         surface = self.write_text(surface, text, text_color, length, height, x, y)
-        self.rect = pygame.Rect(x,y, length, height)
+        self.rect = pygame.Rect(x, y, length, height)
         return surface
 
     def write_text(self, surface, text, text_color, length, height, x, y):
-        font_size = int(length//len(text))
+        font_size = int(length // len(text))
         myFont = pygame.font.SysFont("Calibri", font_size)
         myText = myFont.render(text, 1, text_color)
-        surface.blit(myText, ((x+length/2) - myText.get_width()/2, (y+height/2) - myText.get_height()/2))
+        surface.blit(myText, ((x + length/2) - myText.get_width()/2, (y + height/2) -
+                              myText.get_height()/2))
         return surface
 
     def draw_button(self, surface, color, length, height, x, y, width):
-        for i in range(1,10):
-            s = pygame.Surface((length+(i*2),height+(i*2)))
+        for i in range(1, 10):
+            s = pygame.Surface((length+(i * 2), height+(i * 2)))
             s.fill(color)
-            alpha = (255/(i+2))
+            alpha = (255 / (i + 2))
             if alpha <= 0:
                 alpha = 1
             s.set_alpha(alpha)
-            pygame.draw.rect(s, color, (x-i,y-i,length+i,height+i), width)
-            surface.blit(s, (x-i,y-i))
-        pygame.draw.rect(surface, color, (x,y,length,height), 0)
-        pygame.draw.rect(surface, (190,190,190), (x,y,length,height), 1)
+            pygame.draw.rect(s, color, (x - i, y - i, length + i, height + i), width)
+            surface.blit(s, (x - i, y - i))
+        pygame.draw.rect(surface, color, (x, y, length, height), 0)
+        pygame.draw.rect(surface, (190, 190, 190), (x, y, length, height), 1)
         return surface
 
     def pressed(self, mouse):
