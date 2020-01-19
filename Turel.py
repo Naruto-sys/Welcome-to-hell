98c1bdf5ac616d0184a068705b943f5aea694390
Naruto-sys <gavriil.belov@yandex.ru>
2020-01-19 15:35:18 +0300

    Fix bugs

--- Turel.py
+++ Turel.py
@@ -14,7 +14,6 @@ class Turel(pygame.sprite.Sprite):
 
         self.tile_width = 50
         self.tile_height = 50
-        self.pos_x, self.pos_y = pos_x, pos_y
 
         self.frame = frame
         self.image = frame
@@ -29,76 +28,18 @@ class Turel(pygame.sprite.Sprite):
     def shooting(self, hero, s_x, s_y):
         flag = True
         self.distance = ((hero.rect.x - s_x) ** 2 + (hero.rect.y - s_y) ** 2) ** 0.5
-        self.x = self.start_pos[0] - hero.rect.x
-        self.y = self.start_pos[1] - hero.rect.y
-        x1 = self.hero.rect.x - self.start_pos[0]
-        y1 = self.hero.rect.y - self.start_pos[1]
-        if (x1 ** 2 + y1 ** 2) ** 0.5 < self.distance:
-            while (x1 ** 2 + y1 ** 2) ** 0.5 < self.distance:
-                x1 += self.x / 30
-                y1 += self.y / 30
-
-            self.x = int(x1)
-            self.y = int(y1)
-
-        else:
-            while (x1 ** 2 + y1 ** 2) ** 0.5 < self.distance:
-                x1 += self.x / 30
-                y1 += self.y / 30
-            self.x = -int(x1)
-            self.y = -int(y1)
-
-        if (x1 ** 2 + y1 ** 2) ** 0.5 < self.distance:
-            while (x1 ** 2 + y1 ** 2) ** 0.5 < self.distance:
-                x1 += self.x / 30
-                y1 += self.y / 30
-
-            self.x = int(x1)
-            self.y = int(y1)
-
-        else:
-            while (x1 ** 2 + y1 ** 2) ** 0.5 < self.distance:
-                x1 += self.x / 30
-                y1 += self.y / 30
-            self.x = -int(x1)
-            self.y = -int(y1)
-
-        self.passed_distance = 0
-
-        while True:
-            self.rect.x -= self.x // 30
-            self.rect.y -= self.y // 30
-            self.passed_distance += ((self.x // 30) ** 2 +
-                                     (self.y // 30) ** 2) ** 0.5
-            if self.passed_distance > self.distance:
-                break
-            if self.x < 0:
-                if self.rect.x < self.x + self.start_pos[0]:
-                    break
-            if self.x > 0:
-                if self.rect.x > self.x + self.start_pos[0]:
-                    if self.y > 0:
-                        if self.rect.y > self.y + self.start_pos[1]:
-                            break
-                    if self.y < 0:
-                        if self.rect.y < self.y + self.start_pos[1]:
-                            break
-            for elem in self.walls:
-                if pygame.sprite.collide_mask(self, elem) and elem not in self.enemies_group:
-                    flag = False
-                    break
-        self.rect.x = s_x
-        self.rect.y = s_y
+        if self.distance > 300:
+            flag = False
         if flag:
             self.all_sprites.add(Bullet(load_image("./bullets/bullet.png", -1),
                                         20, 20, (self.rect.x + self.rect.w // 2, self.rect.y + self.rect.h // 2),
                                         (self.hero.rect.x + self.hero.rect.w // 2,
                                          self.hero.rect.y + self.hero.rect.h // 2),
-                                        1000, self.walls, self.hero, self.enemies_group))
+                                        300, self.walls, self.hero, self.enemies_group))
 
     def update(self, *args):
         if self.hp <= 0:
-            self.all_sprites.add(Coin(self.pos_x, self.pos_y, self.hero))
+            self.all_sprites.add(Coin(self.rect.x / 50, self.rect.y / 50, self.hero))
             self.kill()
             self.hero.kills += 1
             return
